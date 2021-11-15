c9a2b42f38bb426762ad7cb54c5f7ca72c100f66
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-15 09:51:19 -0800

    Commit 4

--- app/poros/activity.rb
+++ app/poros/activity.rb
@@ -2,15 +2,19 @@ class Activity
   attr_reader :id, :destination, :forecast, :activities
 
   def initialize(data)
-    require 'pry'; binding.pry
     @id = nil
     @destination = data[:destination]
     @forecast = {
       summary: data[:forecast].current.conditions,
-      temperature: data[:forecast].current.temperature
+      temperature: data[:forecast].current.temperature.to_s + ' F'
     }
-    @activities = {
-      title: data[:activities]
-    }
+    @activities = data[:activities].map do |a|
+      {
+        title: a[:activity],
+        type: a[:type],
+        participants: a[:participants],
+        price: a[:price]
+      }
+    end
   end
 end
