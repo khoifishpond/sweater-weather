112ed620a3d7daadc803a0b4d5ee6ebe62dfb350
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-15 00:16:25 -0800

    Update lng in params

--- spec/poros/geo_code_spec.rb
+++ spec/poros/geo_code_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe GeoCode do
   it 'asfasfasf' do
-    params = {lat: 39.74, long: -104.99}
+    params = {lat: 39.74, lng: -104.99}
     location = GeoCode.new(params)
 
     expect(location.lat).to eq(params[:lat])
-    expect(location.long).to eq(params[:long])
+    expect(location.long).to eq(params[:lng])
   end
 end
