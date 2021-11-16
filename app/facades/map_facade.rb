40a64065d0335b45c24e5f187e6574e3f6a03a5d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-16 13:12:54 -0800

    Update map facade

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -3,4 +3,11 @@ class MapFacade
     data = MapService.get_data(city_state)[:results][0][:locations][0][:latLng]
     GeoCode.new(data)
   end
+
+  def self.roadtrip(start_city, end_city)
+    data = MapService.get_directions(start_city, end_city)[:route]
+    destination = MapFacade.lat_long(end_city)
+    forecast = WeatherFacade.forecast(destination.lat, destination.long)
+    RoadTrip.new(data, forecast)
+  end
 end
