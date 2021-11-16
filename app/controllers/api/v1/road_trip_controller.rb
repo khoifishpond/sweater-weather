ff1f5d526bbd920bc8824d85292393d881cec193
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-16 13:13:38 -0800

    Remove pry

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    require 'pry'; binding.pry
     roadtrip = MapFacade.roadtrip(params[:origin], params[:destination])
     render json: RoadTripSerializer.new(roadtrip)
   end
