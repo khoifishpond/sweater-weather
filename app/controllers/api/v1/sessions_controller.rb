daba61867f5a9f08332dfd6a38fc8a7752f1feec
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-16 01:26:21 -0800

    Create sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    
+    if user && user.authenticate(params[:password])
+      render json: UsersSerializer.new(user)
+    end
+  end
+end
