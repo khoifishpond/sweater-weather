e420a95420013dc796ea4d4d80fe27402142b59a
Khoi Nguyen <khoinguyen311@gmail.com>
2021-11-16 22:03:35 -0800

    Add sad path testing

--- spec/api/v1/user_registration_request_spec.rb
+++ spec/api/v1/user_registration_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'User Registration' do
   it 'creates a user' do
     post '/api/v1/users', params: {
-      email: 'yourname@email.com',
+      email: 'yourNAME@email.com',
       password: 'password',
       password_confirmation: 'password'
     }
@@ -21,5 +21,68 @@ describe 'User Registration' do
     expect(user[:data][:attributes][:email]).to be_a(String)
     expect(user[:data][:attributes]).to have_key(:api_key)
     expect(user[:data][:attributes][:api_key]).to be_a(String)
+  end
+
+  describe 'Sad Paths' do
+    it 'cannot create a user if a field is left blank' do
+      post '/api/v1/users', params: {
+        password: 'password',
+        password_confirmation: 'password'
+      }
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to have_key(:errors)
+      expect(error[:errors]).to be_an(Array)
+      expect(error[:errors][0]).to have_key(:status)
+      expect(error[:errors][0][:status]).to eq('Bad Request')
+      expect(error[:errors][0]).to have_key(:message)
+      expect(error[:errors][0][:message]).to eq('Fields cannot be left blank')
+      expect(error[:errors][0]).to have_key(:code)
+      expect(error[:errors][0][:code]).to eq(400)
+    end
+
+    it 'cannot create a user if a passwords do not match' do
+      post '/api/v1/users', params: {
+        email: 'yourname@email.com',
+        password: 'password',
+        password_confirmation: 'password123'
+      }
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(error).to have_key(:errors)
+      expect(error[:errors]).to be_an(Array)
+      expect(error[:errors][0]).to have_key(:status)
+      expect(error[:errors][0][:status]).to eq('Bad Request')
+      expect(error[:errors][0]).to have_key(:message)
+      expect(error[:errors][0][:message]).to eq('Passwords do not match')
+      expect(error[:errors][0]).to have_key(:code)
+      expect(error[:errors][0][:code]).to eq(400)
+    end
+
+    it 'cannot create a user email is already taken' do
+      params = {
+        email: 'yourname@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+      User.create!(params)
+      post '/api/v1/users', params: {
+        email: 'yourname@email.com',
+        password: 'password123',
+        password_confirmation: 'password123'
+      }
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(error).to have_key(:errors)
+      expect(error[:errors]).to be_an(Array)
+      expect(error[:errors][0]).to have_key(:status)
+      expect(error[:errors][0][:status]).to eq('Bad Request')
+      expect(error[:errors][0]).to have_key(:message)
+      expect(error[:errors][0][:message]).to eq('Email already in use')
+      expect(error[:errors][0]).to have_key(:code)
+      expect(error[:errors][0][:code]).to eq(400)
+    end
   end
 end
