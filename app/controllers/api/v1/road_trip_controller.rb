77efaba4a701268fa965accf2de01352ee1e648e
Khoi Nguyen <khoinguyen311@gmail.com>
2022-07-29 09:40:42 -0700

    check if api key is empty

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    if params[:origin].nil? || params[:destination].nil?
-      render json: error('Bad Request', 'Origin and Destination locations are required', 400)
-    elsif params[:api_key].nil?
+    if params[:api_key].nil? || params[:api_key].empty?
       render json: error('Unauthorized', 'API key is required', 401)
+    elsif params[:origin].nil? || params[:destination].nil?
+      render json: error('Bad Request', 'Origin and Destination locations are required', 400)
     else
       roadtrip = MapFacade.roadtrip(params[:origin], params[:destination])
       render json: RoadTripSerializer.new(roadtrip)
